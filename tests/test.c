9cf9af2987657c9c799f47b67b33a7a72dc0152a
acse-ra922 <110524155+acse-ra922@users.noreply.github.com>
2023-05-13 15:18:27 +0100

    Update test.c

--- tests/test.c
+++ tests/test.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <string.h>
 #include <assert.h>
 
@@ -26,5 +26,41 @@ int main(int argc, char **argv) {
     assert(ntnx_hash_destroy(ctx) == 0);
 
     printf("All tests passed successfully!\n");
+    return 0;
+}
+*/
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include "ntnx_hash.h"
+
+int main() {
+    // Set up ntnx_hash_t context
+    ntnx_hash_t *ctx = ntnx_hash_setup();
+    if (!ctx) {
+        perror("ntnx_hash_setup");
+        exit(EXIT_FAILURE);
+    }
+
+    // Compute hash value of a buffer
+    char buf[] = "Hello, world!";
+    char *hash = ntnx_hash_compute(ctx, buf, strlen(buf));
+    if (!hash) {
+        perror("ntnx_hash_compute");
+        ntnx_hash_destroy(ctx);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Hash value: %s\n", hash);
+
+    // Clean up
+    free(hash);
+    ntnx_hash_destroy(ctx);
+
     return 0;
 }
